140078072e6a1e6a28ebca743b4b0a70034ca769
Smith-Brandon <mormoncook@gmail.com>
2020-03-05 18:51:18 -0800

    Edited SeedData

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -23,39 +23,48 @@ namespace MvcMovie.Models
                 context.Movie.AddRange(
                     new Movie
                     {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-2-12"),
-                        Genre = "Romantic Comedy",
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2001-12-14"),
+                        Genre = "Autobiography ",
                         Rating = "PG 13",
+                        Price = 16.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Christmas in Conway",
+                        ReleaseDate = DateTime.Parse("2013-12-1"),
+                        Genre = "Romantic",
+                        Rating = "PG",
                         Price = 7.99M
                     },
 
                     new Movie
                     {
-                        Title = "Ghostbusters ",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "Comedy",
-                        Rating = "G",
-                        Price = 8.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Ghostbusters 2",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
-                        Genre = "Comedy",
+                        Title = "Christmas Jars",
+                        ReleaseDate = DateTime.Parse("2019-11-4"),
+                        Genre = "Drama",
                         Rating = "G",
                         Price = 9.99M
                     },
 
                     new Movie
                     {
-                        Title = "Rio Bravo",
-                        ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Genre = "Western",
-                        Rating = "R",
-                        Price = 3.99M
-                    }
+                        Title = "Jonah and the Great Fish",
+                        ReleaseDate = DateTime.Parse("2011-11-15"),
+                        Genre = "Family",
+                        Rating = "G",
+                        Price = 14.95M
+                    },
+                    
+                    new Movie
+                     {
+                         Title = "Carthage",
+                         ReleaseDate = DateTime.Parse("2017-9-29"),
+                         Genre = "Drama",
+                         Rating = "PG 13",
+                         Price = 11.99M
+                     }
                 );
                 context.SaveChanges();
             }
